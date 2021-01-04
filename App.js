2ed01497ea270c287747d6ca410fe99d35039812
Iuliu Visovan <iuliu.visovan@usource.ro>
2021-01-04 09:17:40 +0200

    change colors, title and replace with svg pawns

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { Dimensions, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import tictactoe from './src/tictactoe';
+import BlackPawn from './assets/blackPawn';
+import WhitePawn from './assets/whitePawn';
 
 type Props = {};
 export default class App extends Component<Props> {
-  
-  constructor(props){
+  constructor(props) {
     super(props);
 
     tictactoe.start();
@@ -14,8 +15,8 @@ export default class App extends Component<Props> {
     this.state = {
       board: tictactoe.board,
       gameOver: tictactoe.gameover,
-      velha: tictactoe.velha
-    }
+      velha: tictactoe.velha,
+    };
   }
 
   makePlay(index) {
@@ -24,67 +25,59 @@ export default class App extends Component<Props> {
     this.setState({
       board: tictactoe.board,
       gameOver: tictactoe.gameover,
-      velha: tictactoe.velha
+      velha: tictactoe.velha,
     });
   }
 
   resetar() {
-    if(this.state.gameOver === true || this.state.velha === true) {
+    if (this.state.gameOver === true || this.state.velha === true) {
       tictactoe.start();
     }
   }
 
   isGameOver() {
-    if(this.state.gameOver === true) {
+    if (this.state.gameOver === true) {
       return (
         <View style={styles.content}>
-          <Text style={styles.congrats}>
-            Parabéns, você venceu!!
-          </Text>
+          <Text style={styles.congrats}>Parabéns, você venceu!!</Text>
           <TouchableOpacity onPress={this.resetar()} style={styles.button}>
             <Text style={{ color: '#F5F5F5' }}>Resetar</Text>
           </TouchableOpacity>
         </View>
-      )
+      );
     }
   }
 
   isVelha() {
-    if(this.state.velha === true) {
+    if (this.state.velha === true) {
       return (
         <View style={styles.content}>
-          <Text style={styles.congrats}>
-            Deu velha..
-          </Text>
+          <Text style={styles.congrats}>Deu velha..</Text>
           <TouchableOpacity onPress={this.resetar()} style={styles.button}>
             <Text style={{ color: '#F5F5F5' }}>Resetar</Text>
           </TouchableOpacity>
         </View>
-      )
+      );
     }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar style="auto" />
+        <StatusBar style="inverted" />
 
-        <Text style={styles.title}>
-          Jogo da velha
-        </Text>
-
-        <Text style={styles.description}>
-          Desafie os seus amigos e mostre quem é o melhor!
-        </Text>
+        <Text style={styles.title}>Tic tac toe</Text>
 
         <View style={styles.game}>
           {this.state.board.map((value, index) => (
             <TouchableOpacity
               key={index}
               style={styles.piece}
-              onPress={() => {this.makePlay(index)}}
+              onPress={() => {
+                this.makePlay(index);
+              }}
             >
-              <Text style={styles.value}>{value}</Text>
+              {value == 'O' ? <WhitePawn /> : value == 'X' ? <BlackPawn /> : null}
             </TouchableOpacity>
           ))}
         </View>
@@ -98,7 +91,7 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#24135D',
+    backgroundColor: '#bfc3cf',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -107,52 +100,52 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     alignItems: 'center',
     justifyContent: 'center',
-    alignContent: 'center'
+    alignContent: 'center',
   },
   piece: {
     width: Dimensions.get('window').width / 3,
     height: Dimensions.get('window').width / 3,
-    backgroundColor: '#332267',
+    backgroundColor: '#909cae44',
     justifyContent: 'center',
     alignItems: 'center',
-    
+
     borderWidth: 3,
-    borderColor: '#6648C4',
+    borderColor: '#909cae',
     borderRadius: 15,
   },
   value: {
     color: 'red',
-    fontSize: 80
+    fontSize: 80,
   },
   title: {
     fontSize: 35,
-    marginBottom: 10,
+    marginBottom: 16,
     color: '#F5F5F5',
     textAlign: 'center',
     marginLeft: 5,
-    marginRight: 5
+    marginRight: 5,
   },
   congrats: {
     marginTop: 30,
     fontSize: 20,
     color: '#DEDBEC',
     marginBottom: 50,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   button: {
     height: 40,
     width: 186,
     borderWidth: 2,
-    borderColor: '#45E2FF',
+    borderColor: '#909cae',
     borderRadius: 20,
     alignItems: 'center',
     justifyContent: 'center',
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
   description: {
     color: '#DEDBEC',
     textAlign: 'center',
     marginBottom: 20,
     fontSize: 20,
-  }
+  },
 });
